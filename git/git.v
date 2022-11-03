a0a1fb97185af5274721e99b84a6368fe5cbd9cc
Kaio Delphino <dash.zet@gmail.com>
2022-11-03 01:32:17 -0300

    fix: split string to return array

--- git/git.v
+++ git/git.v
@@ -14,12 +14,12 @@ pub fn is_clean_working_tree() bool {
 
 pub fn get_local_branches() []string {
 	result := os.execute("git branch --no-color | sed 's/^[* ] //'")
-	return result.output
+	return result.output.split_any('\n')
 }
 
 pub fn get_remote_branches() []string {
 	result := os.execute("git branch -r --no-color | sed 's/^[* ] //'")
-	return result.output
+	return result.output.split_any('\n')
 }
 
 pub fn get_all_branches() []string {
@@ -35,5 +35,20 @@ pub fn get_all_branches() []string {
 
 pub fn get_all_tags() []string {
 	result := os.execute('git tags')
+	return result.output.split_any('\n')
+}
+
+pub fn get_current_branch() string {
+	result := os.execute('git rev-parse --abbrev-ref HEAD')
 	return result.output
+}
+
+pub fn local_branch_exists(branch string) bool {
+	branches := get_local_branches()
+	return branch in branches
+}
+
+pub fn remote_branch_exists(branch string) bool {
+	branches := get_remote_branches()
+	return branch in branches
 }
