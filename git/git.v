bf49a867f80e11be95eb99c84659e217d6b819e9
Kaio Delphino <dash.zet@gmail.com>
2022-11-03 01:17:35 -0300

    chore: rename functions / code style

--- git/git.v
+++ git/git.v
@@ -12,19 +12,19 @@ pub fn is_clean_working_tree() bool {
 	return result.exit_code == 0
 }
 
-pub fn git_local_branches() []string {
+pub fn get_local_branches() []string {
 	result := os.execute("git branch --no-color | sed 's/^[* ] //'")
 	return result.output
 }
 
-pub fn git_remote_branches() []string {
+pub fn get_remote_branches() []string {
 	result := os.execute("git branch -r --no-color | sed 's/^[* ] //'")
 	return result.output
 }
 
-pub fn git_all_branches() []string {
-	local := git_local_branches()
-	remote := git_remote_branches()
+pub fn get_all_branches() []string {
+	local := get_local_branches()
+	remote := get_remote_branches()
 
 	mut result := []string{cap: local.len + remote.len}
 	result << local
@@ -33,7 +33,7 @@ pub fn git_all_branches() []string {
 	return result
 }
 
-pub fn git_all_tags() []string {
+pub fn get_all_tags() []string {
 	result := os.execute('git tags')
 	return result.output
 }
